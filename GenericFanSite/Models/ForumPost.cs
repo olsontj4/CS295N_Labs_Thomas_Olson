e5971748c95fd138819abb02883a47d1c791f4aa
thomasj041 <thomas.josholson@gmail.com>
2024-10-16 10:45:48 -0700

    New model with comments.

---
+++ GenericFanSite/Models/ForumPost.cs
@@ -0,0 +1,17 @@
+﻿namespace GenericFanSite.Models
+{
+    public class ForumPost
+    {
+        /*    Title of the story
+
+    Topic
+
+    Year the story took place (an integer)
+
+    Text of the story
+
+    Name of the submitter (AppUser)
+
+    Date submitted*/
+    }
+}
