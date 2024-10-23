55bb7d7f148b2d6df8a081271f5fbcc78737dd67
thomasj041 <thomas.josholson@gmail.com>
2024-10-22 18:53:11 -0700

    Merge conflicts.

---
+++ GenericFanSite/Controllers/ForumController.cs
@@ -0,0 +1,25 @@
+﻿using GenericFanSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace GenericFanSite.Controllers
+{
+    public class ForumController : Controller
+    {
+        public IActionResult Index()
+        {
+            ForumPost model = new ForumPost();
+            return View(model);
+        }
+        public IActionResult ForumPostForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ForumPostForm(ForumPost data)
+        {
+            data.ForumDate = DateTime.Now;
+            return View("Index", data);
+        }
+    }
+}
