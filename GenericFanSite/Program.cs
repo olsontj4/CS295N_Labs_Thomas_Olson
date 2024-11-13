67c00e927ddb1a0a82c021d28368e1bcf17c0f22
thomasj041 <thomas.josholson@gmail.com>
2024-11-13 14:15:48 -0800

    .

--- GenericFanSite/Program.cs
+++ GenericFanSite/Program.cs
@@ -1,3 +1,6 @@
+using GenericFanSite.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +15,10 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+var connectionString = builder.Configuration.GetConnectionString("MySqlConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
